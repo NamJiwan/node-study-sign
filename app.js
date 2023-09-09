3fb988898464b5a462d55582a900c24c65db8bc6
NamJiwan <129821189+NamJiwan@users.noreply.github.com>
2023-09-09 21:19:10 +0900

    깃저장소 초기화

---
+++ app.js
@@ -0,0 +1,31 @@
+// const http = require("http");
+// const app = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+//   //   console.log(req.url);
+//   if (req.url === "/") {
+//     res.end("여기는 루트화면입니다.");
+//   } else if (req.url === "/Login") {
+//     res.end("여기는 로그인화면입니다.");
+//   }
+// });
+
+// app.listen(3001, () => {
+//   console.log("http로 가동된 서버입니다.");
+// });
+
+"use strict";
+//모듈
+const express = require("express");
+const app = express();
+
+// 라우팅
+const home = require("./routes/home");
+
+// 앱세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+//use = 미들 웨어를 등록해주는 메서드.
+app.use("/", home);
+
+module.exports = app;
---
+++ bin/www.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const app = require("../app");
+const PROT = 3000;
+// 서버 보여주는거
+app.listen(PROT, () => {
+  console.log("서버저장가동");
+});
